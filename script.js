6a03e392875d267642e56761aca7955f223867d6
Idiaz7 <45011735+ismaeldiaz7@users.noreply.github.com>
2019-11-18 14:12:16 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+var a = document.getElementById("h1");
+var b = document.getElementById("h2");
+var c = document.getElementById("h3");
+var d = document.getElementById("h4");
+var e = document.getElementById("h5");
+var f = document.getElementById("h6");
+var g = document.getElementById("img1");
+var h = document.getElementById("img2");
+var i = document.getElementById("img3");
+var j = document.getElementById("img4");
+var k = document.getElementById("img5");
+var l = document.getElementById("img6");
+var m = document.getElementById("dis1");
+var n = document.getElementById("dis2");
+var o = document.getElementById("dis3");
+var p = document.getElementById("dis4");
+var q = document.getElementById("dis5");
+var r = document.getElementById("dis6");
+
+function toggleAppearboy(){
+	g.classList.toggle("toggle");
+}
+function toggleAppearquack(){
+	h.classList.toggle("toggle");
+}
+
+function toggleAppearcrazy(){
+	i.classList.toggle("toggle");
+}
+
+function toggleAppearflower(){
+	j.classList.toggle("toggle");
+}
+
+function toggleAppearbeauty(){
+	k.classList.toggle("toggle");
+}
+
+function toggleAppearsmile(){
+	l.classList.toggle("toggle");
+}
+
+function changeHiddenboy(){
+	m.innerHTML = "This image to me means thought, and confusion.";
+}
+
+function changeHiddenquack(){
+	n.innerHTML = "QUACK! It's just a duck, appriciate it! ಠ ͜ʖಠ.";
+}
+
+function changeHiddencrazy(){
+	o.innerHTML = "It's just a mental image of some teens today, confused and 'deed'.";
+}
+
+function changeHiddenflower(){
+	p.innerHTML = "It was a quick sketch and the two pedals on the side reminded me of the devil's horn.";
+}
+
+function changeHiddenbeauty(){
+	q.innerHTML = "Up to you! What do you think this means? It's different for every person.";
+}
+
+function changeHiddensmile(){
+	r.innerHTML = "Not everyday will be great and everybody isnt perfect. Like bobby. But keep smiling!";
+}
